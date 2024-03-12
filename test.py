c5fd78ffe6049a46d587727cc69451226c3cf5ea
dhiraj35 <75352708+dhiraj35@users.noreply.github.com>
2024-03-12 12:40:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,12 @@ from redisClass  import *
 
 redisCon = Redis()
 redisCursor = redisCon.connect('fin')
+# r.hset('user-session:123', mapping={
+#     'name': 'John',
+#     "surname": 'Smith',
+#     "company": 'Redis',
+#     "age": 29
+# })
 allData = redisCursor.hgetall('user-session:123')
 print(allData['name'])
 
